2d78aab0ee1c62be3eb2092b7806abdfbd8a4aa3
Bryan Blue <bryanblue@arizona.edu>
2023-04-14 07:17:17 -0700

    linted file and format with black

--- micropython/000db-test-SQL-upload/image0/boot.py
+++ micropython/000db-test-SQL-upload/image0/boot.py
@@ -1,46 +1,50 @@
-# Biosphere 2 remote sensing project
-# Bryan Blue
-# bryanblue@arizona.edu
-# Spring 2023
+"""
+ Biosphere 2 remote sensing project
+ Bryan Blue
+  bryanblue@arizona.edu
+ Spring 2023
+"""
 
-
-try:
-  import usocket as socket
-except:
-  import socket
-
-# import db_post
-# this is a config file to be used to pass values that can change dynamically
-import conf
-print("loaded config")
-
+import gc
+import time
 import esp
+
 esp.osdebug(None)
-import gc  
 from machine import Pin
 import network
 import ntptime
-import time
+
+# this is a config file to be used to pass values that can change dynamically
+import conf
+
+try:
+    import usocket as socket
+except:
+    import socket
 
 gc.collect()
-
 # setup netword connection
 station = network.WLAN(network.STA_IF)
 station.active(True)
 station.connect(conf.WAP_SSID, conf.WAP_PSWD)
-while station.isconnected() == False:
-  pass
-print('Connection successful')
-print(f'STATION: {station.ifconfig()}')
+while station.isconnected() is False:
+    pass
+print("Connection successful")
+print(f"STATION: {station.ifconfig()}")
 
 # set current date time with appropriate offset for timezone -7 is Tucson
 ntptime.host = conf.NTP_HOST
 time.localtime(time.time() + conf.UTC_OFFSET)
 try:
-    print("Local time before synchronization：%s" %str(time.localtime()))
-    #make sure to have internet connection
+    print(f"Local time befor NTP: {str(time.localtime() + conf.UTC_OFFSET)}")
+    # print("Local time before synchronization：%s" % str(time.localtime()))
+    # make sure to have internet connection
     ntptime.settime()
-    print("Local time after synchronization：%s" %str(time.localtime(time.time() + conf.UTC_OFFSET)))
+    print(f"Local time befor NTP: {str(time.localtime() + conf.UTC_OFFSET)}")
+    # print(
+    #     "Local time after synchronization：%s"
+    #     % str(time.localtime(time.time() + conf.UTC_OFFSET))
+    # )
 except:
     print("Error syncing time")
 
