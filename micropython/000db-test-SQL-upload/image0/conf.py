849b3f0ad587badbd3a2d66e7a17b02893e32c27
Bryan Blue <bryanblue@arizona.edu>
2023-04-14 07:19:03 -0700

    formated with black, linted

--- micropython/000db-test-SQL-upload/image0/conf.py
+++ micropython/000db-test-SQL-upload/image0/conf.py
@@ -1,5 +1,8 @@
-# place all configuration constants in this file
-# this file can be uploaded and then system reset
+"""
+ place all configuration constants in this file
+ this file can be uploaded and then system reset
+ """
+
 AUTHOR = "Bryan Blue - bryanblue@arizona.edu"
 VERSION = "23.0.0"
 
@@ -9,10 +12,10 @@ MYNAME = "ESP8266 MicroPython Temperature Server"
 
 
 # NETWORK
-PORT=667
-WAP_SSID = 'DEA_VAN3'
-WAP_PSWD = 'Help1Sago8!MoMo'
+PORT = 667
+WAP_SSID = "DEA_VAN3"
+WAP_PSWD = "Help1Sago8!MoMo"
 
 # TIMESERVER
-NTP_HOST = '3.netbsd.pool.ntp.org'
-UTC_OFFSET = -7 * 60 * 60 # -7 arizona time
+NTP_HOST = "3.netbsd.pool.ntp.org"
+UTC_OFFSET = -7 * 60 * 60  # -7 arizona time
