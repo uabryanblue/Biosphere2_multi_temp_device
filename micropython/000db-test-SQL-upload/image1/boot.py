44038493a0472dbd464f5245bd484331221e8625
Bryan Blue <bryanblue@arizona.edu>
2023-05-03 12:43:37 -0700

    start new image for file datalogger

---
+++ micropython/000db-test-SQL-upload/image1/boot.py
@@ -0,0 +1,53 @@
+"""
+ Biosphere 2 remote sensing project
+ Bryan Blue
+  bryanblue@arizona.edu
+ Spring 2023
+"""
+
+import gc
+import time
+import esp
+
+esp.osdebug(None)
+from machine import Pin
+import network
+import ntptime
+
+# this is a config file to be used to pass values that can change dynamically
+import conf
+
+try:
+    import usocket as socket
+except:
+    import socket
+
+gc.collect()
+# # setup netword connection
+# station = network.WLAN(network.STA_IF)
+# station.active(True)
+# station.connect(conf.WAP_SSID, conf.WAP_PSWD)
+# while station.isconnected() is False:
+#     pass
+# print("Connection successful")
+# print(f"STATION: {station.ifconfig()}")
+
+# # set current date time with appropriate offset for timezone -7 is Tucson
+# ntptime.host = conf.NTP_HOST
+# try:
+#     print(f"Local time before NTP: {str(time.localtime())}")
+#     ntptime.settime()
+#     print(f"Local time after NTP: {str(time.localtime(time.time() + conf.UTC_OFFSET))}")
+# except:
+#     print("Error syncing time")
+
+# initialize pin for led control
+led = Pin(2, Pin.OUT)
+# initialize the led as on
+led.on()
+
+
+
+
+
+   
