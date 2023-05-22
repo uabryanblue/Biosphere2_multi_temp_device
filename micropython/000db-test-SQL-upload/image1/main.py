56f2ef19b81911815a7b36f27d5148fb92b066c5
Bryan Blue <bryanblue@arizona.edu>
2023-05-22 13:50:45 -0700

    test writing sd card

--- micropython/000db-test-SQL-upload/image1/main.py
+++ micropython/000db-test-SQL-upload/image1/main.py
@@ -1,10 +1,22 @@
 import machine
 from time import sleep
+import logger
 
 print("START")
 
 print("good to go")
 
+# LogMount = "/logs/"
+# TestLog =" /logs/testlog.log"
+
+logger.get_storage_stats('/logs')
+
+fn = "/logs/one-line-log.txt"
+with open(fn, "a") as f:
+    n = f.write("blah\nblah2\n")
+    print(n, "bytes writtein")
+
+# logger.write_log("This is a test.", TestLog)
 # while True:
 
 #     sleep(1)
