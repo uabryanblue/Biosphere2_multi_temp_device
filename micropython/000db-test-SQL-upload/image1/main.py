23de6603451a7b2bd3df0cb462bbe4a37f47887e
Bryan Blue <bryanblue@arizona.edu>
2023-05-22 14:40:51 -0700

    added sleep for testing

--- micropython/000db-test-SQL-upload/image1/main.py
+++ micropython/000db-test-SQL-upload/image1/main.py
@@ -1,6 +1,7 @@
 import machine
 from time import sleep
-import logger
+# import logger
+import thermocouple
 
 print("START")
 
@@ -9,12 +10,24 @@ print("good to go")
 # LogMount = "/logs/"
 # TestLog =" /logs/testlog.log"
 
-logger.get_storage_stats('/logs')
+# logger.get_storage_stats('/logs')
 
+t1 = thermocouple.takereading()
+sleep(0.1)
 fn = "/logs/one-line-log.txt"
 with open(fn, "a") as f:
-    n = f.write("blah\nblah2\n")
-    print(n, "bytes writtein")
+    n = f.write("duhuh: ")
+    f.write(f"{t1}")
+    print(n, "bytes written")
+    f.write('\n')
+
+print("dump contents")
+fn = "/logs/one-line-log.txt"
+with open(fn, "r") as f:
+    for line in f:
+        print(line)
+print("end of log")
+
 
 # logger.write_log("This is a test.", TestLog)
 # while True:
