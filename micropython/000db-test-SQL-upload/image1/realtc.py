a967841580005a16cd8ace75769d0fe7f910520c
Bryan Blue <bryanblue@arizona.edu>
2023-05-29 07:18:23 -0700

    internal rtc was not set correctly from DS3231

--- micropython/000db-test-SQL-upload/image1/realtc.py
+++ micropython/000db-test-SQL-upload/image1/realtc.py
@@ -6,22 +6,20 @@
  
 # to set the time on the DS3231 use a tuple as shown here
 # d = DS3231(i2c)
-# d.set_time((YY, MM, DD, wday, hh, mm, ss, 0))
-# example from github
-# set time to 2019, November, 23, Monday, 19 h, 50 min, 0 sec
-# rtc.setDateTime(19, 11, 23, 1, 19, 50, 0)
+# d.set_time((YY, MM, DD, hh, mm, ss, 0, 0))
+# set time to 2023, May, 29, 7 am, 11 minutes, 1 second, NA, NA
+# d.set_time((2023, 05, 29, 7, 11, 1, 0, 0))
 
 from machine import I2C, Pin, RTC
 from ds3231_gen import *
 
 def rtcinit():
     """get the time from the RTC DS3231 board and set the local RTC"""
-
+    rtc = RTC()
     i2c = I2C(sda=machine.Pin(4), scl=machine.Pin(5))
     d = DS3231(i2c)
-    rtc = RTC()
-    print(f"DS3231 time: {d.get_time()}")
-    print(f"local time: {time.localtime()}")
     YY, MM, DD, hh, mm, ss, wday, _ = d.get_time()
     rtc.datetime((YY, MM, DD, wday, hh, mm, ss, 0))
+    print(f"DS3231 time: {d.get_time()}")
+    print(f"local time: {time.localtime()}")
 
