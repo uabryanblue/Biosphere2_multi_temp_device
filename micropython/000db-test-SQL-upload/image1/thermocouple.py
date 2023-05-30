fc5bb87d927f055e6df46d8066cf394d7fe4c2fd
Bryan Blue <bryanblue@arizona.edu>
2023-05-30 09:51:22 -0700

    added  spi.deinit() to release resources

--- micropython/000db-test-SQL-upload/image1/thermocouple.py
+++ micropython/000db-test-SQL-upload/image1/thermocouple.py
@@ -3,6 +3,7 @@ from time import sleep
 
 
 def temp_c(data):
+    """ translate binary response into degreee C """
     temp = data[0] << 8 | data[1]
     if temp & 0x0001:
         return float("NaN")  # Fault reading data.
@@ -46,6 +47,7 @@ def takereading():
         (-0.01053 * temp1 * temp1) + 1.90714 * temp1 - 15.35578
     )  # sensor #1 5/19/2023
 
-    # print(f"TEMP1: {temp1:5} TEMP1 Coorected: {temp1_corrected:5} TEMP2: {temp2:5}")
+    # TODO put in some error checking to ensure spi is released
+    spi.deinit()
 
     return temp1, temp1_corrected, temp2
