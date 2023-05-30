5674da153ba71747dfe2564d1357e0fbc7d0051f
Bryan Blue <bryanblue@arizona.edu>
2023-05-30 10:59:54 -0700

    reading temp values now uses dictionary

--- micropython/000db-test-SQL-upload/image1/main.py
+++ micropython/000db-test-SQL-upload/image1/main.py
@@ -2,6 +2,7 @@ import machine
 from time import sleep
 
 # import logger
+import conf
 import realtc
 import sd
 import thermocouple
@@ -16,12 +17,17 @@ print("good to go")
 log = conf.LOG_MOUNT + "/" + conf.LOG_FILENAME
 
 for i in range(20):
-    t1, t1c, t2 = thermocouple.takereading()
+    # t1, t1c, t2 = thermocouple.takereading()
+    readings = dict()
+    readings = thermocouple.ReadThermocouples(readings)
 
+ 
     with open(log, "a") as f:
-        f.write(f"{i}\t{realtc.formattime(time.localtime())}\t{t1}\t{t1c}\t{t2}\n")
-        # f.write("\n")
-        print(f"{realtc.formattime(time.localtime())}\tt1: {t1:6}\t t1c: {t1c:6}\tt2: {t2:6}")
+        f.write(f"{i}\t{realtc.formattime(time.localtime())}")
+        for key in readings.keys():
+            print(f"key: {key}, value: {readings[key]}")
+            f.write(f"\t{readings[key]}")
+        f.write("\n")
 
     sleep(1)
 
@@ -30,6 +36,9 @@ with open(log, "r") as f:
     for line in f:
         print(line.rstrip())
 print("end of log")
+
+
+
 
 # ########### !!! if you don't close it, it will get overwritten
 # when the next PYMAKR update is performed!!!!!!!!!!!
--- micropython/000db-test-SQL-upload/image1/thermocouple.py
+++ micropython/000db-test-SQL-upload/image1/thermocouple.py
@@ -12,42 +12,82 @@ def temp_c(data):
         temp -= 16384  # Sign bit set, take 2's compliment.
     return temp * 0.25
 
-
-def takereading():
-    """
-    take readings of all configured thermocouples
-    use callibration equation to adjust to more precise readings
-    TODO: this should be turned into a digital mux
-    """
-
-    data = bytearray(4)
-    spi = machine.SPI(1, baudrate=5000000, polarity=0, phase=0)
-    # need one pin for each chip select
-    cs = machine.Pin(16, machine.Pin.OUT)
-    cs2 = machine.Pin(0, machine.Pin.OUT) 
-
+def readthermocouple(CSPin, spi):
+    raw_data = bytearray(4)
+    cs = machine.Pin(CSPin, machine.Pin.OUT)
     # chip select needs to be low to take a reading, init high 
     cs.on()
-    cs2.on()
 
     # read sensor and convert to degrees C
     cs.off()
-    spi.readinto(data)
+    spi.readinto(raw_data)
     cs.on()
-    temp1 = temp_c(data)
+    temp = temp_c(raw_data)
 
-    # read sensor and convert to degrees C
-    cs2.off()
-    spi.readinto(data)
-    cs2.on()
-    temp2 = temp_c(data)
+    return temp
 
-    # callibrate each thermocouple and use corrected value
-    temp1_corrected = (
-        (-0.01053 * temp1 * temp1) + 1.90714 * temp1 - 15.35578
-    )  # sensor #1 5/19/2023
 
+def ReadThermocouples(readings):
+    """ setup spi connection, read all thermocouples, close spi connection """
+
+    spi = machine.SPI(1, baudrate=5000000, polarity=0, phase=0)
+    readings = dict()
+    readings[16] = 0.0
+    readings[0] = 0.0
+    print(readings)
+
+    for key in readings.keys():
+        CSPin = key
+        readings[key] = readthermocouple(CSPin, spi)
+
+    # CSPin = 16 # and 0 work right now
+    callibration = []
+    # temp1 = readthermocouple(CSPin, spi)
+    # temp1_corrected = 0.0
+    
+    # CSPin = 0
+    # temp2 = readthermocouple(CSPin, spi)
     # TODO put in some error checking to ensure spi is released
     spi.deinit()
 
-    return temp1, temp1_corrected, temp2
+    return readings
+
+# old code, remove later
+# def takereading():
+#     """
+#     take readings of all configured thermocouples
+#     use callibration equation to adjust to more precise readings
+#     TODO: this should be turned into a digital mux
+#     """
+
+#     data = bytearray(4)
+#     spi = machine.SPI(1, baudrate=5000000, polarity=0, phase=0)
+#     # need one pin for each chip select
+#     cs = machine.Pin(16, machine.Pin.OUT)
+#     cs2 = machine.Pin(0, machine.Pin.OUT) 
+
+#     # chip select needs to be low to take a reading, init high 
+#     cs.on()
+#     cs2.on()
+
+#     # read sensor and convert to degrees C
+#     cs.off()
+#     spi.readinto(data)
+#     cs.on()
+#     temp1 = temp_c(data)
+
+#     # read sensor and convert to degrees C
+#     cs2.off()
+#     spi.readinto(data)
+#     cs2.on()
+#     temp2 = temp_c(data)
+
+#     # callibrate each thermocouple and use corrected value
+#     temp1_corrected = (
+#         (-0.01053 * temp1 * temp1) + 1.90714 * temp1 - 15.35578
+#     )  # sensor #1 5/19/2023
+
+#     # TODO put in some error checking to ensure spi is released
+#     spi.deinit()
+
+#     return temp1, temp1_corrected, temp2
