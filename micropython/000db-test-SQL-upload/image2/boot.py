bb28019ef2c92822273087da13eb20c539d791bd
Bryan Blue <bryanblue@arizona.edu>
2023-05-31 14:51:28 -0700

    image2 t reader init commit

---
+++ micropython/000db-test-SQL-upload/image2/boot.py
@@ -0,0 +1,31 @@
+"""
+Biosphere 2 remote sensing project
+AUTHOR: Bryan Blue
+EMAIL: bryanblue@arizona.edu
+STARTED: 2023
+"""
+
+import gc
+import time
+import esp
+import conf
+from machine import Pin
+import realtc
+import sd
+from time import sleep
+
+# esp.osdebug(None)
+
+
+# pushes first real line of output
+# to the line after the terminal garbage finishes
+# "garbage" is due to mismatch in terminal speed on boot, not a bug
+print("booting")
+
+# set the on board RTC to the time from the DS3231
+# realtc.rtcinit()
+# print("set time")
+
+# attach SD card module and mount the SD card, if one is present
+# sd.initSD(conf.LOG_MOUNT)
+# sleep(0.5)
---
+++ micropython/000db-test-SQL-upload/image2/main.py
@@ -0,0 +1,56 @@
+import machine
+from time import sleep
+
+# import logger
+import conf
+import realtc
+import sd
+import thermocouple
+
+print("START")
+
+print("good to go")
+
+
+# sleep(0.1)
+# log = conf.LOG_MOUNT + "/" + conf.LOG_FILENAME
+
+
+
+# TODO this needs to be read from configuration
+readings = dict()
+# readings[16] = 0.0
+# readings[0] = 0.0
+readings[1] = 0.0
+readings[2] = 0.0
+
+
+for i in range(10):
+    readings = thermocouple.read_thermocouples(readings)
+  
+    # for key in readings.keys():
+    #     print(f"key: {key}, value: {readings[key]}")
+
+
+    # with open(log, "a") as f:
+    #     f.write(f"{i}\t{realtc.formattime(time.localtime())}")
+    #     for key in readings.keys():
+    #         print(f"key: {key}, value: {readings[key]}")
+    #         f.write(f"\t{readings[key]}")
+    #     f.write("\n")
+
+    sleep(1)
+
+# print("dump contents")
+# with open(log, "r") as f:
+#     for line in f:
+#         print(line.rstrip())
+# print("end of log")
+
+
+
+
+# ########### !!! if you don't close it, it will get overwritten
+# when the next PYMAKR update is performed!!!!!!!!!!!
+# sd.closeSD(conf.LOG_MOUNT)
+
